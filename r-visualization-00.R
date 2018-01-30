0a8817a80353482fba4eb9c12da5b45aa224c358
Linh B Ngo <lngo@clemson.edu>
2018-01-30 11:18:08 -0500

    Initial commit

---
+++ r-visualization-00.R
@@ -0,0 +1,12 @@
+# clear environment
+rm(list = ls())
+
+# load tidyver, a comprehensive data analytic package
+install.packages('tidyverse')
+
+# install data sets (we will only load data as neccessary
+install.packages(c('babynames','fueleconomy','nasaweather','nycflights13'))
+
+
+
+
---
+++ r-visualization-01-correlation.R
@@ -0,0 +1,17 @@
+# clear environment
+rm(list = ls())
+
+# load tidyver, a comprehensive data analytic package
+library('tidyverse')
+
+# what does tidyverse have?
+sessionInfo()
+
+# load data set
+library(babynames)
+
+# In correlation
+
+
+
+
